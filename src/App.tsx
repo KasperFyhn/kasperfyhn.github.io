7ce326bac6bb10fd715e70d4e1db6892b1b810db
Kasper Fyhn <kfyhn@cas.au.dk>
2024-11-01 14:43:10 +0100

    Navbar, colors and some links

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
 import "./App.css";
-import {HashRouter, Route, Routes} from "react-router-dom";
+import "./common.css";
+import LinkedIn from "./logos/LI-In-Bug.png";
+import GitHub from "./logos/github-mark.png";
+import {HashRouter, Link, Route, Routes, useLocation} from "react-router-dom";
+import React, {PropsWithChildren} from "react";
+import {AboutPage} from "./about/AboutPage";
+
+
+function NavBar() {
+    const {pathname} = useLocation();
+    const parts = pathname.split("/");
+    const paths = [];
+    for (let i = 1; i < parts.length; i++) {
+        if (!parts[i]) {
+            continue;
+        }
+        paths.push({name: parts[i], url: parts.slice(0, i + 1).join("/")});
+    }
+
+    return (<div className={'navbar'}>
+        {/*<button>MENU</button>*/}
+        <div>
+            <Link to={"/"}><b>Home </b></Link>
+            {/*{paths.map((item) => <Link to={item.url}>/ {item.name}</Link>)}*/}
+        </div>
+
+
+        <Link to={"/about"}>About</Link>
+
+        <Link to={"https://www.linkedin.com/in/kasper-fyhn/"} target={"_blank"}>
+            <img className={'white-image'} height={"20px"} src={LinkedIn} alt={"LinkedIn"}/>
+        </Link>
+        <Link to={"https://github.com/KasperFyhn"} target={"_blank"}>
+            <img className={'white-image'} height={"20px"} src={GitHub} alt={"LinkedIn"}/>
+        </Link>
+    </div>)
+}
 
 
 function FrontPage() {
@@ -10,12 +46,25 @@ function FrontPage() {
     );
 }
 
+
+const Content: React.FC<PropsWithChildren> = ({children}) => {
+    return (<div className={'content__container'}>
+        <div className={'content__pane'}>
+            {children}
+        </div>
+    </div>)
+}
+
+
 function App() {
     return (
         <HashRouter>
+            <NavBar/>
             <div className="root">
+
                 <Routes>
                     <Route path="/" element={<FrontPage/>}/>
+                    <Route path="/about" element={<Content><AboutPage/></Content>}/>
                 </Routes>
             </div>
         </HashRouter>
---
+++ src/about/AboutPage.tsx
@@ -0,0 +1,8 @@
+
+
+export const AboutPage = () => {
+    return <div>
+        <h1>About me</h1>
+        <p>My name is Kasper. I am a PhD student at Aarhus University. There is not much to see here yet.</p>
+    </div>
+}
