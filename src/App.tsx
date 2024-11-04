67bdb6a616c34e20a5269344bf8914832c1b111a
Kasper Fyhn <kfyhn@cas.au.dk>
2024-11-04 19:29:45 +0100

    Adding prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,73 +2,88 @@ import "./App.css";
 import "./common.css";
 import LinkedIn from "./logos/LI-In-Bug.png";
 import GitHub from "./logos/github-mark.png";
-import {HashRouter, Link, Route, Routes, useLocation} from "react-router-dom";
-import React, {PropsWithChildren} from "react";
-import {AboutPage} from "./about/AboutPage";
-
+import { HashRouter, Link, Route, Routes, useLocation } from "react-router-dom";
+import React, { PropsWithChildren } from "react";
+import { AboutPage } from "./about/AboutPage";
 
 function NavBar() {
-    const {pathname} = useLocation();
-    const parts = pathname.split("/");
-    const paths = [];
-    for (let i = 1; i < parts.length; i++) {
-        if (!parts[i]) {
-            continue;
-        }
-        paths.push({name: parts[i], url: parts.slice(0, i + 1).join("/")});
+  const { pathname } = useLocation();
+  const parts = pathname.split("/");
+  const paths = [];
+  for (let i = 1; i < parts.length; i++) {
+    if (!parts[i]) {
+      continue;
     }
+    paths.push({ name: parts[i], url: parts.slice(0, i + 1).join("/") });
+  }
 
-    return (<div className={'navbar'}>
-        {/*<button>MENU</button>*/}
-        <div>
-            <Link to={"/"}><b>Home </b></Link>
-            {/*{paths.map((item) => <Link to={item.url}>/ {item.name}</Link>)}*/}
-        </div>
-
-
-        <Link to={"/about"}>About</Link>
-
-        <Link to={"https://www.linkedin.com/in/kasper-fyhn/"} target={"_blank"}>
-            <img className={'white-image'} height={"20px"} src={LinkedIn} alt={"LinkedIn"}/>
+  return (
+    <div className={"navbar"}>
+      {/*<button>MENU</button>*/}
+      <div>
+        <Link to={"/"}>
+          <b>Home </b>
         </Link>
-        <Link to={"https://github.com/KasperFyhn"} target={"_blank"}>
-            <img className={'white-image'} height={"20px"} src={GitHub} alt={"LinkedIn"}/>
-        </Link>
-    </div>)
+        {/*{paths.map((item) => <Link to={item.url}>/ {item.name}</Link>)}*/}
+      </div>
+
+      <Link to={"/about"}>About</Link>
+
+      <Link to={"https://www.linkedin.com/in/kasper-fyhn/"} target={"_blank"}>
+        <img
+          className={"white-image"}
+          height={"20px"}
+          src={LinkedIn}
+          alt={"LinkedIn"}
+        />
+      </Link>
+      <Link to={"https://github.com/KasperFyhn"} target={"_blank"}>
+        <img
+          className={"white-image"}
+          height={"20px"}
+          src={GitHub}
+          alt={"LinkedIn"}
+        />
+      </Link>
+    </div>
+  );
 }
-
 
 function FrontPage() {
-    return (
-        <div className="frontpage">
-            <h1>kasperfyhn.github.io</h1>
-        </div>
-    );
+  return (
+    <div className="frontpage">
+      <h1>kasperfyhn.github.io</h1>
+    </div>
+  );
 }
 
-
-const Content: React.FC<PropsWithChildren> = ({children}) => {
-    return (<div className={'content__container'}>
-        <div className={'content__pane'}>
-            {children}
-        </div>
-    </div>)
-}
-
+const Content: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={"content__container"}>
+      <div className={"content__pane"}>{children}</div>
+    </div>
+  );
+};
 
 function App() {
-    return (
-        <HashRouter>
-            <NavBar/>
-            <div className="root">
-
-                <Routes>
-                    <Route path="/" element={<FrontPage/>}/>
-                    <Route path="/about" element={<Content><AboutPage/></Content>}/>
-                </Routes>
-            </div>
-        </HashRouter>
-    );
+  return (
+    <HashRouter>
+      <NavBar />
+      <div className="root">
+        <Routes>
+          <Route path="/" element={<FrontPage />} />
+          <Route
+            path="/about"
+            element={
+              <Content>
+                <AboutPage />
+              </Content>
+            }
+          />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/about/AboutPage.tsx
+++ src/about/AboutPage.tsx
@@ -1,8 +1,11 @@
-
-
 export const AboutPage = () => {
-    return <div>
-        <h1>About me</h1>
-        <p>My name is Kasper. I am a PhD student at Aarhus University. There is not much to see here yet.</p>
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>
+        My name is Kasper. I am a PhD student at Aarhus University. There is not
+        much to see here yet.
+      </p>
     </div>
-}
+  );
+};
